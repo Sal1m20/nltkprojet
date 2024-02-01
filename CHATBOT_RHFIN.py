2161c58abe451d532b1b684b56bae673322b8945
Sal1m20 <71647830+Sal1m20@users.noreply.github.com>
2024-02-01 15:45:56 +0000

    Update CHATBOT_RHFIN.py

--- CHATBOT_RHFIN.py
+++ CHATBOT_RHFIN.py
@@ -5,6 +5,8 @@ from nltk.stem import WordNetLemmatizer
 import string
 import streamlit as st
 import nltk
+import nltk
+nltk.download('wordnet')
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
